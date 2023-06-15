16077c3772b889b8b0357992a8ee5a2649b25db7
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-06-15 16:11:24 -0300

    Add hero section to index.html

--- main.js
+++ main.js
@@ -36,3 +36,28 @@ navbarLinks.forEach(navbarlink => {
         closeNavbar();
     });
 });
+
+/* Header active when windows scroll down to 100px */
+
+const header = document.querySelector(".header");
+const backTopBtn = document.querySelector(".back-top-btn");
+
+const showElementOnScroll = () => {
+    if(window.scrollY > 100){
+        header.classList.add("active");
+        backTopBtn.classList.add("active");
+    } else {
+        header.classList.remove("active");
+        backTopBtn.classList.remove("active");
+    }
+}
+
+window.addEventListener('scroll', () => {
+    showElementOnScroll();  
+});
+
+
+
+
+
+
