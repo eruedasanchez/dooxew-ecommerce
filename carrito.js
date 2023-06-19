6a9f5df7a74140d28385684a0fdd71e8b5f79009
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-06-19 12:59:48 -0300

    Apply Sweet Alert in buy button

--- carrito.js
+++ carrito.js
@@ -7,6 +7,9 @@ productsInBag = JSON.parse(productsInBag);
 
 
 
+
+
+
 const emptyBag = document.querySelector("#empty-bag");   // emptyBagContainer
 const productCard = document.querySelector("#product-card"); // #bag-products
 // bag-actions seria amount
@@ -17,8 +20,13 @@ const payBtn = document.querySelector("#pay-btn"); // bag-actions-buy
 
 const subtotal = document.getElementById("subtotal");
 const iva = document.getElementById("iva");
-const envio = document.getElementById("envio");
+const shipping = document.getElementById("shipping");
 const total = document.getElementById("total");
+const payAmount = document.getElementById("payAmount");
+
+
+
+
 
 
 function uploadPurchaseOrder(){
@@ -43,12 +51,12 @@ function uploadPurchaseOrder(){
                 <h4 class="product-name">${product.name}</h4>
                 <div class="wrapper">
                     <div class="product-qty">
-                        <button class="cart-button" id="decrement"><ion-icon class="cart-ion-icon" name="remove-outline"></ion-icon></button>
+                        <button class="cart-button cart-button-minus" id="minus"><ion-icon class="cart-ion-icon" name="remove-outline"></ion-icon></button>
                         <span class=".cart-span" id="quantity">${product.cant}</span>
-                        <button class="cart-button" id="increment"><ion-icon class="cart-ion-icon" name="add-outline"></ion-icon></button>
+                        <button class="cart-button cart-button-plus" id="plus"><ion-icon class="cart-ion-icon" name="add-outline"></ion-icon></button>
                     </div>
                     <div class="price">
-                        $ <span class=".cart-span" id="price">${product.price * product.cant}</span>
+                        $ <span class=".cart-span" id="price">${product.price * product.cant}.00</span>
                     </div>
                 </div>
             </div>
@@ -58,6 +66,18 @@ function uploadPurchaseOrder(){
             `;
             
             productCard.append(div);
+
+            const plusBtn = document.querySelector("#plus");
+            let quantity = document.querySelector("#quantity");
+            let price = document.querySelector("#price");
+
+            let lastValue = parseInt(quantity.innerText);
+            let lastPrice = parseInt(price.innerText);
+
+            plusBtn.addEventListener('click', () => {
+                lastValue++;
+                lastPrice += 20;
+            });
         })
     } else {
         emptyBag.classList.remove("disabled"); 
@@ -65,14 +85,20 @@ function uploadPurchaseOrder(){
         // bagActionsContainer.classList.add("disabled");
         // finishPurchaseContainer.classList.add("disabled");
     }
-
     refreshDeleteButtons();
     refreshTotal();
 }
 
 const refreshTotal = () => {
-    const totalAmmount = productsInBag.reduce((acc, producto) => acc + (producto.price * producto.cant), 0);
+    const subtotalAmmount = Math.round(parseInt(productsInBag.reduce((acc, producto) => acc + (producto.price * producto.cant), 0)));
+    const ivaTax = Math.round(parseInt(subtotalAmmount * 0.21));
+    const shippingTax = Math.round(parseInt(subtotalAmmount * 0.03));
+    subtotal.innerText = `${subtotalAmmount}`;
+    iva.innerText = `${ivaTax}`;
+    shipping.innerText = `${shippingTax}`;
+    const totalAmmount = subtotalAmmount + ivaTax + shippingTax;
     total.innerText = `${totalAmmount}`; 
+    payAmount.innerText = `${totalAmmount}`;
 }
 
 const refreshDeleteButtons = () => {
@@ -93,4 +119,53 @@ const deleteOfBag = (event) => {
     localStorage.setItem("products-in-bag", JSON.stringify(productsInBag));
 }
 
+
+
 uploadPurchaseOrder();
+
+// confirmar compra 
+
+payBtn.addEventListener("click", () => {
+    if(parseInt(payAmount.innerText) === 0){
+        Swal.fire({
+            icon: 'error',
+            title: 'El carrito esta vacio!',
+            showConfirmButton: true,   // true muestra el boton de ok y false lo oculta
+            // timer: 2000 // milisegundos
+        })
+    } else {
+        Swal.fire({
+            title: 'Desea solicitar su pedido?',
+            text: "Presione solicitar pedido para confirmar su compra",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: 'hsl(27, 46%, 58%)',
+            cancelButtonColor: '#9ca3b0',
+            confirmButtonText: 'Solicitar pedido'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Compra confirmada!',
+                    'Nos encontramos en Avenida Rivadavia 5700. Nuestros horarios de atencion son lunes a viernes de 9 a 18hs. Gracias por confiar en nostros',
+                    'success'
+                )
+
+                productsInBag.length = 0;
+                localStorage.setItem("products-in-bag", JSON.stringify(productsInBag));
+
+                emptyBag.classList.remove("disabled"); 
+                productCard.classList.add("disabled");
+
+                subtotal.innerText = 0;
+                iva.innerText = 0; 
+                shipping.innerText = 0;
+                total.innerText = 0;
+                payAmount.innerText = 0;
+            }
+        })
+    }
+});
+
+
+
+
