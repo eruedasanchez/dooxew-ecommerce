1f23292fe96c85e503d574b4bad085be56788c49
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-06-20 11:26:33 -0300

    Add like products

--- main.js
+++ main.js
@@ -1,69 +1,8 @@
-/* Define products in array products */
-
-// class Product{
-//     constructor(name, id, image, categoryName, categoryId, price){
-//         this.name = name;
-//         this.id = id;
-//         this.image = image;
-//         this.categoryName = categoryName;
-//         this.categoryId = categoryId;
-//         this.price = price;
-//     }
-// }
-
-// const products = [];
-
-// const firstAccesory = new Product("Art Deco Home", "accesory-01", "./assets/img/accesory-01.jpg", "accesory", "accesories", 30);
-// const secondAccesory = new Product("Dark Green Jug", "accesory-02", "./assets/img/accesory-02.jpg", "accesory", "accesories", 17);
-// const thirdAccesory = new Product("Drinking Glasses", "accesory-03", "./assets/img/accesory-03.jpg", "accesory", "accesories", 21); 
-// const fourthAccesory = new Product("High Quality Glass Bottle", "accesory-04", "./assets/img/accesory-04.jpg", "accesory", "accesories", 30);
-// const fifthAccesory = new Product("Living Room & Bedroom Lights", "accesory-05", "./assets/img/accesory-05.jpg", "accesory", "accesories", 45);
-// const sixthAccesory = new Product("Teapot With Black Tea", "accesory-06", "./assets/img/accesory-06.jpg", "accesory", "accesories", 25);
-// const seventhAccesory = new Product("Wooden Cups", "accesory-07", "./assets/img/accesory-07.jpg", "accesory", "accesories", 29);
-
-// const firstDecoration = new Product("Animi Dolor Pariatur", "decoration-01", "./assets/img/decoration-01.jpg", "decoration", "decorations", 10);
-// const secondDecoration = new Product("Artificial Potted Plant", "decoration-02", "./assets/img/decoration-02.jpg", "decoration", "decorations", 40);
-// const thirdDecoration = new Product("Smooth Disk", "decoration-03", "./assets/img/decoration-03.jpg", "decoration", "decorations", 46);
-// const fourthDecoration = new Product("Unique Decoration", "decoration-04", "./assets/img/decoration-04.jpg", "decoration", "decorations", 15);
-// const fifthDecoration = new Product("Vase Of Flowers", "decoration-05", "./assets/img/decoration-05.jpg", "decoration", "decorations", 77);
-// const sixthDecoration = new Product("Wood Eggs", "decoration-06", "./assets/img/decoration-06.jpg", "decoration", "decorations", 19);
-// const seventhDecoration = new Product("Wooden Box", "decoration-07", "./assets/img/decoration-07.jpg", "decoration", "decorations", 27);
-
-// const firstFurniture = new Product("Helen Chair", "furniture-01", "./assets/img/furniture-01.jpg", "furniture", "furnitures", 69);
-// const secondFurniture = new Product("Nancy Chair", "furniture-02", "./assets/img/furniture-02.jpg", "furniture", "furnitures", 90);
-// const thirdFurniture = new Product("Simple Chair", "furniture-03", "./assets/img/furniture-03.jpg", "furniture", "furnitures", 40);
-// const fourthFurniture = new Product("Table Black", "furniture-04", "./assets/img/furniture-04.jpg", "furniture", "furnitures", 67);
-// const fifthFurniture = new Product("Table Wood Pine", "furniture-05", "./assets/img/furniture-05.jpg", "furniture", "furnitures", 50);
-
-// products.push(firstDecoration);
-// products.push(firstAccesory);
-// products.push(secondDecoration);
-// products.push(secondAccesory);
-
-// products.push(thirdAccesory);
-// products.push(firstFurniture);
-// products.push(fourthAccesory);
-// products.push(fifthAccesory);
-
-// products.push(secondFurniture);
-// products.push(thirdFurniture);
-// products.push(thirdDecoration);
-// products.push(fourthFurniture);
-
-// products.push(fifthFurniture);
-// products.push(sixthAccesory);
-// products.push(fourthDecoration);
-// products.push(fifthDecoration);
-
-// products.push(sixthDecoration);
-// products.push(seventhDecoration);
-// products.push(seventhAccesory);
-
-import products from './data.json' assert{type: 'json'};
+import products from './data.json' assert{type: 'json'}; // Consumir datos de la API local
 
 const btnCategory = document.querySelectorAll(".filter-btn");
 const productsContainer = document.querySelector("#data-filter");
-let btnAdd = document.querySelectorAll(".card-bag-btn");   // Seleccion de todos los botones "bag"
+let btnAdd = document.querySelectorAll(".card-bag-btn");  
 const quantity = document.querySelector("#ctn-badge");
 
 
@@ -81,20 +20,19 @@ function addToBag(event){
         productsInBag[index].cant++;
     } else {
         /* El producto es agregado al carrito por primera vez */
-        addedProduct.cant = 1;                                                      // Se agrega propiedad cantidad a los productos
+        addedProduct.cant = 1;                                                  // Se agrega propiedad cantidad a los productos
         productsInBag.push(addedProduct);
     }
 
     refreshQuantity();
     
-    /* Se guarda al arreglo en el local Storage para cargarlo desde carrito.html */ 
-    localStorage.setItem("products-in-bag", JSON.stringify(productsInBag));
+    localStorage.setItem("products-in-bag", JSON.stringify(productsInBag));    // Guarda al arreglo en el local Storage para cargarlo desde carrito.html
 }
 
 /**** Refresh quantity ****/
 
 const refreshQuantity = () => {
-    let newQuantity = productsInBag.reduce((acc, product) => acc + product.cant, 0);   // Se cuenta la cantidad de productos del arreglo y como los repetidos se almacenan en cant, se aplica reduce 
+    let newQuantity = productsInBag.reduce((acc, product) => acc + product.cant, 0);   // Cuenta la cantidad de productos del arreglo y como los repetidos se almacenan en cant, se aplica reduce 
     quantity.innerText = newQuantity;
 }
 
@@ -107,15 +45,17 @@ const refreshAddBtn = () => {
     })
 }
 
-/* Load products */ 
+/**** Load products ****/ 
 
 const load = (productsChosen) => {
-    productsContainer.innerHTML = "";           // Vacia el contenedor para que no vaya acumulando las categorias filtradas dado que se le aplica un append
+    productsContainer.innerHTML = "";           // Vacia el contenedor para que no vaya acumulando las categorias filtradas porque se aplica un append
+    
     productsChosen.forEach(product => {
         const li = document.createElement("li");
         const productCat = product.categoryName; 
         li.classList.add(productCat);
         if(product.id === "decoration-01"){
+            /* Producto con el 10% descuento */
             li.innerHTML = `
             <div class="product-card">
                 <div class="card-banner img-holder has-before" style="--width: 300; --height: 300;">
@@ -160,6 +100,7 @@ const load = (productsChosen) => {
             </div>
             `;
         } else if(product.id === "accesory-01"){
+            /* Producto que no cuenta con stock */
             li.innerHTML = `
             <div class="product-card">
                 <div class="card-banner img-holder has-before" style="--width: 300; --height: 300;">
@@ -190,6 +131,7 @@ const load = (productsChosen) => {
             </div>
         `;
         } else {
+            /* Todos los demas productos */
             li.innerHTML = `
                 <div class="product-card">
                     <div class="card-banner img-holder has-before" style="--width: 300; --height: 300;">
@@ -230,6 +172,7 @@ const load = (productsChosen) => {
 } 
 
 /************************************************** END FUNCTIONS ***************************************************/
+
 load(products);
 
 /***** Navbar toggle *****/
@@ -307,23 +250,20 @@ btnCategory.forEach(btn => {
     })
 });
 
-/**** Like products to the bag ****/
+/**** Like products ****/
 
-let cora = 0;
-const ctnHeart = document.querySelector('#ctn-heart');
-let heartBtns = document.querySelectorAll(".card-heart-btn");
+let likesCounter = 0;  
+const likeCtn = document.querySelector('#ctn-heart'); 
+const likeBtns = document.querySelectorAll(".card-heart-btn");
 
-heartBtns.forEach(btn => {
+likeBtns.forEach(btn => {
     btn.addEventListener("click", () => {
-        cora++;
-        ctnHeart.innerText = cora;
+        likesCounter++;
+        likeCtn.innerText = likesCounter;
     });
 })
 
-
-
-
-
+/**** Load products in Local Storage ****/
 
 let productsInBag;
 let productsInBagLocalStorage = localStorage.getItem("products-in-bag");
@@ -334,14 +274,4 @@ if(productsInBagLocalStorage){
 } else {
     productsInBag = [];
 }
-
-
-
-
-
-
-
-
-
-
 
